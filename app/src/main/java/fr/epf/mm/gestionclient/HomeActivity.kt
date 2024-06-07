ddfac049d32cf5d71924b62ae4e02227ccb1604a
maxence <135508615+MaxenceJRY@users.noreply.github.com>
2024-06-07 15:50:01 +0200

    feat: traduction de l'appli (#6)

--- app/src/main/java/fr/epf/mm/gestionclient/HomeActivity.kt
+++ app/src/main/java/fr/epf/mm/gestionclient/HomeActivity.kt
@@ -1,12 +1,14 @@
 package fr.epf.mm.gestionclient
-
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
-import androidx.room.Room
 import fr.epf.mm.gestionclient.API.AppDatabase
 import fr.epf.mm.gestionclient.API.DatabaseProvider
 import fr.epf.mm.gestionclient.model.Country
@@ -14,22 +16,29 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import android.widget.RadioGroup
+import java.util.Locale
 
 class HomeActivity : AppCompatActivity() {
 
-    private var selectedLanguage = "en"
     private lateinit var appDatabase: AppDatabase
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private var selectedLanguage: String
+        get() = sharedPreferences.getString("language", "fr") ?: "fr"
+        set(value) = sharedPreferences.edit().putString("language", value).apply()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        setAppLanguage(selectedLanguage) // Définir la langue par défaut
         setContentView(R.layout.activity_home)
+
+
+        appDatabase = DatabaseProvider.getInstance(this)
 
         val listButton = findViewById<Button>(R.id.home_list_button)
         val editText = findViewById<EditText>(R.id.searchCountry_editText)
         val getAllButton = findViewById<Button>(R.id.get_all_button)
-
-        appDatabase = DatabaseProvider.getInstance(this)
 
         listButton.setOnClickListener {
             val query = editText.text.toString()
@@ -42,18 +51,49 @@ class HomeActivity : AppCompatActivity() {
             }
         }
 
-        val radioGroup = findViewById<RadioGroup>(R.id.radioGroupLangue)
-        radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            selectedLanguage = when (checkedId) {
-                R.id.radioButtonFr -> "fr"
-                R.id.radioButtonEn -> "en"
-                R.id.radioButtonEs -> "es"
-                else -> "fr"
-            }
-        }
         getAllButton.setOnClickListener {
             displayAllItemsFromDatabase()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.list_countries, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item1 -> {
+                updateLanguage("fr")
+                true
+            }
+            R.id.item2 -> {
+                updateLanguage("en")
+                true
+            }
+            R.id.item3 -> {
+                updateLanguage("es")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateLanguage(languageCode: String) {
+        selectedLanguage = languageCode // Sauvegarde de la langue sélectionnée
+        setAppLanguage(languageCode) // Définir la langue avec la nouvelle sélection
+
+        val refresh = Intent(this, HomeActivity::class.java)
+        finish()
+        startActivity(refresh)
+    }
+
+    private fun setAppLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
     }
 
     private fun displayAllItemsFromDatabase() {
@@ -75,6 +115,5 @@ class HomeActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
-
     }
 }
